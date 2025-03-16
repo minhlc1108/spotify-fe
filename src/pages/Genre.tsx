9f5ef30b6e0a2380ad5a0380c5f0a164b0a21cac
minhlc1108 <minh.lcm.2003@gmail.com>
2025-03-16 21:13:47 +0700

    route page

---
+++ src/pages/Genre.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Genre: React.FC = () => {
+	return <div>Genre</div>;
+};
+
+export default Genre;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,17 @@
 import MainLayout from "@/layouts/MainLayout";
+import Account from "@/pages/Account";
+import Album from "@/pages/Album";
+import Artist from "@/pages/Artist";
+import Genre from "@/pages/Genre";
 import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import NotFound from "@/pages/NotFound";
+import Playlist from "@/pages/Playlist";
+import Register from "@/pages/Register";
+import Search from "@/pages/Search";
+import Section from "@/pages/Section";
+import Track from "@/pages/Track";
+import User from "@/pages/User";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,11 +19,21 @@ const AppRouter: React.FC = () => {
 	return (
 		<Router>
 			<Routes>
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
 				<Route path="/" element={<MainLayout />}>
 					<Route index element={<Home />} />
-					<Route path="home" element={<Home />} />
-					<Route path="artist" element={<div>Artist</div>} />
+					<Route path="album/:id" element={<Album />} />
+					<Route path="artist/:id" element={<Artist />} />
+					<Route path="account" element={<Account />} />
+					<Route path="genre/:id" element={<Genre />} />
+					<Route path="playlist/:id" element={<Playlist />} />
+					<Route path="search/:keyword" element={<Search />} />
+					<Route path="section/:id" element={<Section />} />
+					<Route path="track/:id" element={<Track />} />
+					<Route path="user/:username" element={<User />} />
 				</Route>
+				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
 		</Router>
 	);
