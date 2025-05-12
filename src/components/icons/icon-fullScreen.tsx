e7039f8932d19cd59cec335e60dc0276f699dcca
minhlc1108 <minh.lcm.2003@gmail.com>
2025-05-13 00:05:35 +0700

    play video

--- src/components/icons/icon-fullScreen.tsx
+++ src/components/icons/icon-fullScreen.tsx
@@ -1,4 +1,6 @@
-const FullScreenIcon = (): JSX.Element => (
+import { SVGProps } from "react";
+
+const FullScreenIcon = (props: SVGProps<SVGSVGElement>): JSX.Element => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		viewBox="0 0 24 24"
@@ -6,6 +8,7 @@ const FullScreenIcon = (): JSX.Element => (
 		stroke="currentColor"
 		strokeWidth="1.5"
 		className="w-6 h-6"
+		{...props}
 	>
 		<path
 			d="M4 4h6M4 4v6M20 4h-6M20 4v6M4 20h6M4 20v-6M20 20h-6M20 20v-6"
